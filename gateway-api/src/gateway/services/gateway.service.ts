ac4b32663d9b5c3a426bd54ba2cae93b5db10045
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2023-06-15 17:06:10 +0700

    fix: gateway clientproxy

--- gateway-api/src/gateway/services/gateway.service.ts
+++ gateway-api/src/gateway/services/gateway.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateGateWay } from '../dto/gate-handle.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { SERVICES_ENUM } from 'src/core/enums/services.enum';
+import { CLIENT_METHOD_MICROSERVICE } from 'src/core/enums/client-method.microservice.enum';
 @Injectable()
 export class GatewayService {
   constructor(
@@ -9,15 +10,28 @@ export class GatewayService {
     private readonly acountClient: ClientProxy,
   ) {}
   async connect(data: CreateGateWay) {
-    return await this.getClient(data.service)[data.method](
-      { cmd: data.cmd },
-      data,
-    );
+    console.log(this.acountClient, 'acount');
+
+    return data.method === CLIENT_METHOD_MICROSERVICE.EMIT
+      ? async () => {
+          await this.getClient(data.service)[data.method](
+            { cmd: data.cmd },
+            data,
+          );
+          return true;
+        }
+      : await this.getClient(data.service)[data.method](
+          { cmd: data.cmd },
+          data,
+        );
   }
   getClient(service: SERVICES_ENUM): ClientProxy {
+    const acounts = this.acountClient;
     const clients = {
-      acount: this.acountClient,
+      acounts: acounts,
     };
+    console.log(clients[service], 'clients[service]');
+
     return clients[service];
   }
 }
